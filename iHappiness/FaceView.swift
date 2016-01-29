a9b153332cc5cb6644d8a8910537ff7b734e2f2c
nikrb <nik_rb@yahoo.com>
2016-01-29 21:51:08 +0000

    add delegation to set smiliness for view from controller

--- iHappiness/FaceView.swift
+++ iHappiness/FaceView.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
+// could be FaceViewDelegate, but we're only using it for data
+// if protocol isn't restricted to class, we get an error weak cannot be applied to non-class type
+// when we declare the pointer as weak var dataSource:FaceViewDataSource below
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView( sender: FaceView) -> Double?
+}
+
+// draw in storyboard designer!
+@IBDesignable
 class FaceView: UIView {
     // by default rotating the view "scale to fill" will draw an ellipse :(
     // to stop the scale to fill, select view then change view attribute view mode to "redraw" from "scale to fill"
     // observe change to spark redraw
+    // @IBInspectable puts a property into the design view attribute inspector
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet{ setNeedsDisplay()}}
+    @IBInspectable
     var colour: UIColor = UIColor.blueColor() {didSet{ setNeedsDisplay()}}
+    @IBInspectable
     var scale: CGFloat = 0.90 { didSet { setNeedsDisplay()}}
     
     var faceCentre : CGPoint{
@@ -22,6 +35,26 @@ class FaceView: UIView {
     }
     var faceRadius: CGFloat{
         return min(bounds.size.width, bounds.size.height)/2 * scale
+    }
+    
+    // using the protocol
+    // weak because this and caller reference each other giving circular dependency to will never leave memory
+    // it means the pointer should not be used to keep it in memory
+    weak var dataSource: FaceViewDataSource?
+    
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCentre, radius: faceRadius, startAngle: 0, endAngle: CGFloat( 2*M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        colour.set() // sets fill and stroke
+        facePath.stroke()
+        
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        
+        // ?? notation: if the lhs is not nil else use 0.0
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
+        let smilePath = bezierPathForSmile(smiliness)
+        smilePath.stroke()
     }
     
     private enum Eye { case Left, Right }
@@ -69,19 +102,5 @@ class FaceView: UIView {
         path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
         path.lineWidth = lineWidth
         return path
-    }
-    
-    override func drawRect(rect: CGRect) {
-        let facePath = UIBezierPath(arcCenter: faceCentre, radius: faceRadius, startAngle: 0, endAngle: CGFloat( 2*M_PI), clockwise: true)
-        facePath.lineWidth = lineWidth
-        colour.set() // sets fill and stroke
-        facePath.stroke()
-        
-        bezierPathForEye(.Left).stroke()
-        bezierPathForEye(.Right).stroke()
-        
-        let smiliness = -0.5
-        let smilePath = bezierPathForSmile(smiliness)
-        smilePath.stroke()
     }
 }
