b573db00eb16255065d2b021cb21676297c5d681
nikrb <nik_rb@yahoo.com>
2016-01-29 17:27:04 +0000

    prevent draw scale to fit on screen rotation as we get an ellipse not a circular face

--- iHappiness/FaceView.swift
+++ iHappiness/FaceView.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class FaceView: UIView {
+    // by default rotating the view "scale to fill" will draw an ellipse :(
+    // to stop the scale to fill, select view then change view attribute view mode to "redraw" from "scale to fill"
     // observe change to spark redraw
     var lineWidth: CGFloat = 3 { didSet{ setNeedsDisplay()}}
     var colour: UIColor = UIColor.blueColor() {didSet{ setNeedsDisplay()}}
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay()}}
     
     var faceCentre : CGPoint{
         // for a read only prop we don't need the get {}, just return 'only' get {
         return convertPoint(center, fromView: superview)
     }
     var faceRadius: CGFloat{
-        return min(bounds.size.width, bounds.size.height)/2
+        return min(bounds.size.width, bounds.size.height)/2 * scale
     }
     override func drawRect(rect: CGRect) {
         let facePath = UIBezierPath(arcCenter: faceCentre, radius: faceRadius, startAngle: 0, endAngle: CGFloat( 2*M_PI), clockwise: true)
